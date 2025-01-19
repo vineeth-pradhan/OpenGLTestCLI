d49798a567a96b73e500e6b00d96fc1601ed896a
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2025-01-19 14:59:54 +0530

    Inital setup for an OpenGL test project

---
+++ HelloWorld.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <glfw3.h>
+/** glad.c already provides this header, therefore
+ignoring this include to avoid conflicts
+#include <glad/glad.h>
+*/
+#include <KHR/khrplatform.h>
+
+int main()
+{
+  std::cout << "Everything went fine!! \n";
+}
