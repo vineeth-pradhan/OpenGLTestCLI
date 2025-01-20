cc622b2e8b567315f020be71b4ccecf6809e6358
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2025-01-20 13:52:54 +0530

    Render a blank window

--- HelloWorld.cc
+++ HelloWorld.cc
@@ -1,12 +1,57 @@
 #include <iostream>
-#include <glfw3.h>
-/** glad.c already provides this header, therefore
-ignoring this include to avoid conflicts
 #include <glad/glad.h>
-*/
 #include <KHR/khrplatform.h>
+#include <GLFW/glfw3.h>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// g++ -o main HelloWorld.cc glad.c -I/Users/vineeth/Projects/CPPIncludes/OpenGL -L/Users/vineeth/Projects/CPPIncludes/OpenGL/Libs -lglfw3 -framework Cocoa -framework IOKit
 int main()
 {
+  if(glfwInit() == 0) {
+    std::cout << "Failed to load \n";
+    return -1;
+  }
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL) {
+    std::cerr << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+  glViewport(0, 0, 800, 600);
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // render loop
+  while(!glfwWindowShouldClose(window)) {
+    // input
+    processInput(window);
+
+    // rendering commands
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // check and call events and swap buffers
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  glfwTerminate();
   std::cout << "Everything went fine!! \n";
+  return 0;
 }
